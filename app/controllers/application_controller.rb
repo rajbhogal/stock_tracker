093cb4d1951a1df25b625a95efbf5d403152b72d
rajinder bhogal <you@example.com>
2021-08-25 02:28:36 +0100

    Added self referential association - users and friends

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected 
+  protected
 
-    def configure_permitted_parameters
-     devise_parameter_sanitizer(:sign_up, keys: [:first_name, :last_name])
-     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-    end 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end  
 
 end
